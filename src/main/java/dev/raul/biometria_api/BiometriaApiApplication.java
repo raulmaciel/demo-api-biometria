226023ec4869ec4e92fdbd7cb83aaaedd06e8542
raulmaciel <raulmacielxd@gmail.com>
2025-06-09 11:28:07 -0300

    initial commit

---
+++ src/main/java/dev/raul/biometria_api/BiometriaApiApplication.java
@@ -0,0 +1,13 @@
+package dev.raul.biometria_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiometriaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiometriaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/raul/biometria_api/BiometriaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.raul.biometria_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiometriaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
